b886c922ec83554313e04a9eda546effb354a0fb
Saurabh-314 <maxxxjob314@gmail.com>
2023-02-25 20:16:51 +0530

    add environment variable

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 
   useEffect(() => {
     fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US"
+      process.env.REACT_APP_BASE_API
     )
       .then((res) => res.json())
       .then((data) => setPopularMovies(data.results));
